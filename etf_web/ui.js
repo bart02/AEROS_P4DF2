8fb81c798f71429caa9e5e1332e8a0185c8051ef
Artem Batalov <a@batalov.me>
2021-01-08 23:05:30 +0700

    Start beautification

--- etf_web/ui.js
+++
@@ -1,7 +0,0 @@
-function disableButtons(p) {
-    Array.from(document.getElementsByClassName("but")).forEach(
-        function (element, index, array) {
-            element.disabled = p;
-        }
-    );
-}
