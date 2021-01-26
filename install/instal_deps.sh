c2c18a666e4a58a36ba0dbb7d9cc2a62e08c5cbb
Artem Batalov <a@batalov.me>
2021-01-26 21:50:44 +0300

    Final install deps script

--- install/instal_deps.sh
+++ install/instal_deps.sh
@@ -1,6 +1,4 @@
+echo 'deb [trusted=yes] http://repo.etf.batalov.me debian/' | sudo tee -a /etc/apt/sources.list
 sudo apt update
-# sudo apt install ros-melodic-laser-geometry
-# cd ~/catkin_ws/src
-# sudo apt install ros-melodic-cmake-modules
-# sudo apt install ros-melodic-roslint 
+# sudo apt install ros-melodic-laser-geometry ros-melodic-cmake-modules ros-melodic-roslint
 sudo apt install ros-melodic-pointcloud-to-laserscan ros-melodic-rplidar-ros
